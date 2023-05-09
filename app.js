833dae799fa60c4d2ad81c830346032d806caa90
kusud-yuki <u.b_bfg@icloud.com>
2023-05-09 16:53:51 +0900

    背景に波線が動くjsの作成（未完成）

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 
+// canvas要素の取得
+const canvas = document.getElementById('canvas');
+
+// コンテキストの取得
+const ctx = canvas.getContext('2d');
+
+// 画面サイズの取得
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+// canvasサイズの指定
+canvas.width = width;
+canvas.height = height;
+
+// パラメータの設定
+const wave = {
+  y: height / 2,
+  length: 0.01,
+  amplitude: 100,
+  frequency: 0.01
+};
