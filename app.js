117d0bd301eaab5f35ea41fec9d34d8f42471dda
kusud-yuki <u.b_bfg@icloud.com>
2023-05-17 16:55:34 +0900

    今まで描画して来た波や〇等を消すボタンを作成

--- app.js
+++ app.js
@@ -1,121 +1,126 @@
-// canvas要素の取得
-const canvas = document.getElementById('canvas');
-const mouse = {
-    x: null,
-    y: null
-  };
-
-// 現在の日時を取得
-const currentDate = new Date();
-
-// 日時を表示する要素を取得
-const dateElement = document.getElementById('date');
-
-// 日時を表示するテキストを作成
-const dateText = `現在の日時は${currentDate.toLocaleString()}です。`;
-
-// 日時を表示する要素にテキストを設定
-dateElement.textContent = dateText;
-
-// コンテキストの取得
-const ctx = canvas.getContext('2d');
-canvas.addEventListener('mousemove', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-  });
+  // canvas要素の取得
+  const canvas = document.getElementById('canvas');
+  const mouse = {
+      x: null,
+      y: null
+    };
   
-// 画面サイズの取得
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-// canvasサイズの指定
-canvas.width = width;
-canvas.height = height;
-
-// パラメータの設定
-const wave = {
-  y: height / 2,
-  length: 0.01,
-  amplitude: 100,
-  frequency: 0.01
-};
-
-let increment = wave.frequency;
-function drawCircle() {
+  // 現在の日時を取得
+  const currentDate = new Date();
+  
+  // 日時を表示する要素を取得
+  const dateElement = document.getElementById('date');
+  
+  // 日時を表示するテキストを作成
+  const dateText = `現在の日時は${currentDate.toLocaleString()}です。`;
+  
+  // 日時を表示する要素にテキストを設定
+  dateElement.textContent = dateText;
+  
+  // コンテキストの取得
+  const ctx = canvas.getContext('2d');
+  canvas.addEventListener('mousemove', function(event) {
+      mouse.x = event.x;
+      mouse.y = event.y;
+    });
+    
+  // 画面サイズの取得
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  
+  // canvasサイズの指定
+  canvas.width = width;
+  canvas.height = height;
+  
+  // パラメータの設定
+  const wave = {
+    y: height / 2,
+    length: 0.01,
+    amplitude: 100,
+    frequency: 0.01
+  };
+  
+  let increment = wave.frequency;
+  function drawCircle() {
+      ctx.beginPath();
+      ctx.arc(mouse.x, mouse.y, 20, 0, 2 * Math.PI, false);
+      ctx.fillStyle = circleColor;
+      ctx.fill();
+    }
+  // テキストの設定
+  let textContent = 'Hello, World!';
+  function drawText() {
+    ctx.font = '24px sans-serif';
+    ctx.fillStyle = '#000000';
+    ctx.fillText(textContent, mouse.x + 30, mouse.y + 30);
+  }
+  // 四角形の設定
+  let squareSize = 100;
+  function drawSquare() {
+    ctx.fillStyle = '#00FF00';
+    ctx.fillRect(
+      (canvas.width - squareSize) / 2,
+      (canvas.height - squareSize) / 2,
+      squareSize,
+      squareSize
+    );
+  }
+  window.clearCanvas = function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  
+    // クリックイベントの追加
+  let circleColor = '#ff0000';
+  let clickCount = 0;
+  
+  canvas.addEventListener('click', function() {
+    circleColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    clickCount++;
+    textContent = `Clicked: ${clickCount} times`;
+    squareSize = Math.floor(Math.random() * 150) + 50;
+  
+  });
+  // グラデーション円の設定
+  function drawGradientCircle() {
+    const gradient = ctx.createRadialGradient(mouse.x, mouse.y, 10, mouse.x, mouse.y, 20);
+    gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
+    gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
     ctx.beginPath();
     ctx.arc(mouse.x, mouse.y, 20, 0, 2 * Math.PI, false);
-    ctx.fillStyle = circleColor;
+    ctx.fillStyle = gradient;
     ctx.fill();
   }
-// テキストの設定
-let textContent = 'Hello, World!';
-function drawText() {
-  ctx.font = '24px sans-serif';
-  ctx.fillStyle = '#000000';
-  ctx.fillText(textContent, mouse.x + 30, mouse.y + 30);
-}
-// 四角形の設定
-let squareSize = 100;
-function drawSquare() {
-  ctx.fillStyle = '#00FF00';
-  ctx.fillRect(
-    (canvas.width - squareSize) / 2,
-    (canvas.height - squareSize) / 2,
-    squareSize,
-    squareSize
-  );
-}
-
-  // クリックイベントの追加
-let circleColor = '#ff0000';
-let clickCount = 0;
-canvas.addEventListener('click', function() {
-  circleColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-  clickCount++;
-  textContent = `Clicked: ${clickCount} times`;
-  squareSize = Math.floor(Math.random() * 150) + 50;
-
-});
-// グラデーション円の設定
-function drawGradientCircle() {
-  const gradient = ctx.createRadialGradient(mouse.x, mouse.y, 10, mouse.x, mouse.y, 20);
-  gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
-  gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 20, 0, 2 * Math.PI, false);
-  ctx.fillStyle = gradient;
-  ctx.fill();
-}
-
-function changeAmplitude() {
-  wave.amplitude = Math.floor(Math.random() * 150) + 50;
-}
-// アニメーションの描画
-function animate() {
-  // 背景の描画
-  ctx.fillStyle = '#ffffff';
-  ctx.fillRect(0, 0, width, height);
-
-  // 波の描画
-  ctx.beginPath();
-  ctx.moveTo(0, wave.y);
-  for (let i = 0; i < width; i++) {
-    ctx.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude);
-  }
-  ctx.strokeStyle = '#000000';
-  ctx.stroke();
-  drawCircle();
-  // 波の動きを設定
-  increment += wave.frequency;
-
-  drawGradientCircle();
-
-  drawText();
-
-  drawSquare();
-
-  requestAnimationFrame(animate);
-}
-
-animate();
   
+  function changeAmplitude() {
+    wave.amplitude = Math.floor(Math.random() * 150) + 50;
+  }
+  let isAnimating = true;
+  // アニメーションの描画
+  function animate() {
+    if (!isAnimating) return;
+    // 背景の描画
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, width, height);
+  
+    // 波の描画
+    ctx.beginPath();
+    ctx.moveTo(0, wave.y);
+    for (let i = 0; i < width; i++) {
+      ctx.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude);
+    }
+    ctx.strokeStyle = '#000000';
+    ctx.stroke();
+    drawCircle();
+    // 波の動きを設定
+    increment += wave.frequency;
+  
+    drawGradientCircle();
+  
+    drawText();
+  
+    drawSquare();
+  
+    requestAnimationFrame(animate);
+  }
+  
+  animate();
